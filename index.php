b071c54d03da8028705df6b01bb86c4c7c4a46cd
leo00062 <carpentierleo2001@gmail.com>
2025-03-21 08:42:12 +0100

    Update

--- index.php
+++ index.php
@@ -5,20 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <script type="module" src="./main.js"></script> -->
     <script src="./main.js" defer></script>
+    <link rel="stylesheet" href="style.css">
 
     <title>Document</title>
 </head>
 
 <body>
-    <div id="root">
-        <!-- A réativer pour bases_1 -->
-        <!-- <ul>
-            <li class="link linked" id="link"></li>
-            <li class="link"></li>
-            <li class="link"></li>
-        </ul> -->
-        <!-- Fin A réativer pour bases_1 -->
-    </div>
+    <div id="show"></div>
+    <main id="main">
+        <div id="root">
+            <!-- A réativer pour bases_1 -->
+            <!-- <ul>
+                <li class="link linked" id="link"></li>
+                <li class="link"></li>
+                <li class="link"></li>
+            </ul> -->
+            <!-- Fin A réativer pour bases_1 -->
+        </div>
+    </main>
     <!-- <script src="./main.js"></script> -->
 </body>
 
--- main.js
+++ main.js
@@ -1,1 +1,50 @@
-// TODO:
+let lien = document.createElement("a");
+lien.setAttribute("href", "#");
+// lien.setAttribute("target", "_blank");
+// lien.innerHTML = "<Visiter mon site>"; // permet de rajouter de la saisie HTML A NE PAS FAIRE // DONTDOTHIS
+lien.textContent = "Visiter mon site "; // Permets d'ajouter du text ! //SAUVEGARDE
+// lien.style.textDecoration = "none";
+// lien.style.backgroundColor = "blue";
+// lien.style.borderRadius = "20px";
+// lien.style.color = "white";
+
+let divRoot = document.querySelector("#root");
+
+// lien.addEventListener("mouseover", () => {
+//   lien.classList.toggle("perso");
+// });
+
+lien.addEventListener("mouseenter", () => {
+  lien.classList.add("perso");
+});
+
+lien.addEventListener("mouseout", () => {
+  lien.classList.remove("perso");
+});
+
+root.append(lien);
+// lien.remove(lien); // Pour enlever  nom de ce que l'on veut enlever . remove //SAUVEGARDE:
+
+// console.log(lien);
+let scrollPos = 0; //SAUVEGARDE
+let show = document.querySelector("#show");
+
+window.document.addEventListener("scroll", () => {
+  if (document.body.getBoundingClientRect().top > scrollPos) {
+    lien.style.transform = "rotate(-45deg)";
+    show.textContent = `Vous scroller vers le haut vous etes a ${scrollY} px du haut `;
+    console.log(scrollY);
+  } else if (scrollY > 100) {
+    show.textContent = `Vous etes allez trop loin !`;
+  } else {
+    lien.style.transform = "rotate(90deg)";
+    show.textContent = `Vous scroller vers le bas vous etes a  ${scrollY} px du bas `;
+    console.log(scrollY);
+  }
+
+  scrollPos = document.body.getBoundingClientRect().top;
+});
+
+lien.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
